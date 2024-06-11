a5983745c631880953f271f9510380f771caf8e0
Nicole Apono <napononicole@gmail.com>
2024-06-11 14:11:35 +0300

    define the structure of the User and Task tables in your SQLite database.

---
+++ lib/models/task.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    priority = Column(Integer)
+    due_date = Column(Date)
+    assigned_to = Column(Integer, ForeignKey('users.id'))
+
+    assignee = relationship("User", back_populates="tasks")
